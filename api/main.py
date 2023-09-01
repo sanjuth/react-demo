deb0f763e3d61b68efdc7f81fbba23bd321e7318
sanjuth <sanjuthreddy2003@gmail.com>
2023-09-01 21:03:53 +0530

    initial commit

---
+++ api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Create an instance of the FastAPI class
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define a route with a GET request
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [resp, setResp] = useState();
+
+  const handleClick = () =>{
+    var requestOptions = {
+      method: 'GET',
+    };
+    
+    fetch("http://localhost:8000/", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result.message)
+        setResp(result.message)
+      })
+      .catch(error => console.log('error', error));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          REACT JS !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={handleClick}>click here</button>
+
+        { resp &&
+          <p>
+            RESPONSE FROM API : {resp}
+          </p>
+        }
+        
       </header>
     </div>
   );
